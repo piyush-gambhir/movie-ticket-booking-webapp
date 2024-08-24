ccf9291aa65dc735a3c1380a90c4d28496ca90ba
vasujain275 <vasujain275@gmail.com>
2024-08-24 17:40:29 +0530

    Feat: Added Movies Page Basic Layout

---
+++ app/(app)/movies/[id]/page.js
@@ -0,0 +1,25 @@
+import MoviePage from "@/components/MoviePage";
+
+async function getMovieData(id) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/v1/movies/${id}`,
+    {
+      cache: "no-store", // Fetches fresh data on each request
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch movie data");
+  }
+
+  return res.json();
+}
+
+export default async function MoviePageRoute({ params }) {
+  const movieData = await getMovieData(params.id);
+
+  // Assuming you have a list of theaters to pass
+  const theaters = []; // Replace with actual theater data if needed
+
+  return <MoviePage movieData={movieData} theaters={theaters} />;
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    domains: ["image.tmdb.org"], // Add the domain here
+  },
 };
 
 export default nextConfig;
